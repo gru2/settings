3a2cf3fc1829749ae543772454fb0991dab1e4cc
gru2 <Radovan.Obradovic@gmail.com>
2018-03-18 16:04:29 +0100

    cammel case.

--- bin/createCppClass.py
+++ bin/createCppClass.py
@@ -1,94 +1,88 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-""" Tool to create new class. """
+""" Tool to create new cpp class. """
 
 import sys
 import os
 import string
+import re
 
+def camelCaseSplit(identifier):
+    matches = re.finditer('.+?(?:(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
+    return [m.group(0) for m in matches]
 
-hFileTemplate = """#ifndef __$CLASS_NAME$_H
-#define __$CLASS_NAME$_H
+def camelCaseUpper(identifier):
+    l = camelCaseSplit(identifier)
+    l2 = [x.upper() for x in l]
+    return "_".join(l2)
+
+hFileTemplate = """#ifndef $CLASS_NAME$_H
+#define $CLASS_NAME$_H
 
 class $className$
 {
 public:
 	$className$();
 	~$className$();
-
-protected:
 };
 
 #endif
 """
 
-cppFileTemplate = """
-#include <$className$.h>
+cppFileTemplate = """#include <$className$.h>
 
-$className$::$className$()
-{
-}
-
-$className$::~$className$()
-{
-}
-
+$className$::$className$() { }
+$className$::~$className$() { }
 """
 
-testFileTemplate = """
-#include <$className$.h>
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
+testFileTemplate = """#include <$className$.h>
+#include <Usutf.h>
 
-void fail()
-{
-	cout << "TEST_FAILED\\n";
-	exit(1);
-}
-
-void test_$className$()
+USUTF_TEST(test$className$)
 {
 	$className$ x;
-	cout << "TEST_PASSED\\n";
-}
-
-int main(int argc, char *argv[])
-{
-	test_$className$();
-	return 0;
+	Usutf::test(true);
 }
 """
 
 def main():
 
-    if len(sys.argv) != 2:
-        print("ussage: createClass.py <class_name>")
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        print("ussage: createClass.py [-t] <class_name>")
         sys.exit(1)
 
-    className = sys.argv[1]
+    generateTest = False
+    for x in sys.argv:
+        if x == "-t":
+            generateTest = True
+        else:
+            className = x
+    
+    classNameUpper = camelCaseUpper(className)
     hFileName = className + ".h"
     cppFileName = className + ".cpp"
     testFileName = className + "Test.cpp"
     
     hFileContents = hFileTemplate.replace("$className$", className)
-    hFileContents = hFileContents.replace("$CLASS_NAME$", className.upper())
+    hFileContents = hFileContents.replace("$CLASS_NAME$", classNameUpper)
     cppFileContents = cppFileTemplate.replace("$className$", className)
     testFileContents = testFileTemplate.replace("$className$", className)
 
-    print(hFileName, cppFileName)
-
+    print("creatingFile:", os.getcwd() + "/" + hFileName)
     hFile = open(hFileName, "w")
-    cppFile = open(cppFileName, "w")
-    testFile = open(testFileName, "w")
-    
     hFile.write(hFileContents)
-    cppFile.write(cppFileContents)
-    testFile.write(testFileContents)
-    
     hFile.close()
+
+    print("creatingFile:", os.getcwd() + "/" + cppFileName)
+    cppFile = open(cppFileName, "w")
+    cppFile.write(cppFileContents)
     cppFile.close()
-    testFile.close()
+
+    if generateTest:
+        print("creatingFile:", os.getcwd() + "/" + testFileName)
+        testFile = open(testFileName, "w")    
+        testFile.write(testFileContents)    
+        testFile.close()
+
 
 main()
-
